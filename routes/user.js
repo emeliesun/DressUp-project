ea6e1c4c33445f6c56c85dd9a0657cecf5a9bf69
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-03-23 16:10:15 +0100

    temporary commit

---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const User = require('../models/user');
+
+
+// Directs to /user/signup
+// app.get(/)
+
+
+// Directs to /user/signup-confirm
+
+// Directs to /user/login
+
+// Directs to /user/dashboard
+
+// Directs to /user/feed
+
+// Directs to /user/add-friend
+
+// Directs to /user/logout
+
+
