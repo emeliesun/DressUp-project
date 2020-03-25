dec6341dff161cd8d657af0bf572befe330d0561
Upward-Spiral <azadeh.farshidi@gmail.com>
2020-03-25 14:16:59 +0100

    changes to app.js regarding ptotect

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
-
+debugger
 // Partials
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -36,10 +36,6 @@ hbs.registerHelper('isEqual', (value1, value2)=> {
     
 })
 
-// Middleware Setup
-// app.use('/user/dashboard', protect);
-// app.use('/outfit', protect);
-// app.use('/item', protect);
 
 
 // Database connection
@@ -67,22 +63,30 @@ mongoose
    })
  );
 
+
+// Middleware Setup
+app.use('/user/dashboard', protect);
+app.use('/outfit', protect);
+app.use('/item', protect);
+
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/user', require('./routes/user'));
 app.use('/outfit', require('./routes/outfit'));
 app.use('/item', require('./routes/item'));
 
+
+
 // Function defenitions
-// middleware definition
-// function protect (req,res,next){ 
-//   debugger
-//   if (req.session.currentUser) next()
-//   else { res.redirect('/user/login', {
-//     errorMessage: "Login Required!"
-//     }); 
-//   }
-// }
+//middleware definition
+function protect (req,res,next){ 
+  debugger
+  if (req.session.currentUser) next()
+  else { res.redirect('/user/login', {
+    errorMessage: "Login Required!"
+    }); 
+  }
+}
 
 // Listener
 app.listen(3002, () => {
