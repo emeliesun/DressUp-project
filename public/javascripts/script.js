46297b3efff64fb8cb9ad025a5d95a33be47d5be
Upward-Spiral <azadeh.farshidi@gmail.com>
2020-03-25 20:23:12 +0100

    succeded in adding friends. Must work on doing it via page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,12 @@
-const navscript = require('../javascripts/nav');
+const navJs = require('../javascripts/nav');
+const outfitJs = require('../javascripts/outfit');
+const userJs = require('../javascripts/user');
+const itemJs = require('../javascripts/item');
+const axios = require('axios');
+
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+
 
 // const $loginBtn = document.getElementById('login-btn');
 // $loginBtn.addEventListener('click', (event)=>{
--- routes/user.js
+++ routes/user.js
@@ -81,7 +81,7 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  debugger
+  // debugger
     const {username, password} = req.body;
     if (username === '' || password === ''){
             res.render('user/login', {
@@ -115,11 +115,11 @@ app.post('/login', (req, res) => {
     });
 });
 
-// Dashboard (Home)
+// Home
 app.get('/home', (req, res) => {
-  debugger
+  // debugger
   let userName = req.session.currentUser.username;
-  res.render('user/dashboard',{
+  res.render('user/home',{
     welcomeMessage: `Welcome ${userName}`
   })
 });
@@ -134,12 +134,22 @@ app.get('/feed', (req, res) => {
     project: {
      username:1,
      friend_request: 1,
-     friends: 1
+     friends: 1,
+     new_reqs:1
     }
    })
    .populate("friend_request")
    .populate("friends")
     .then(feedData => {
+      User.findOneAndUpdate({username: userName},{
+        new_reqs: false
+        })
+        .then(theUser => {
+          console.log('new feed flag was reset!')
+        })
+        .catch(err => {
+          res.send(`Error: ${err}`);
+        });
       res.render('user/feed', {userFeedData:feedData});
     })
     .catch(err => {
@@ -147,9 +157,50 @@ app.get('/feed', (req, res) => {
     });
 });
 
-//  Render /user/add-friend
-app.get('/add_friend', (req, res) => {
-  User.find()
+// Settings
+app.get('/user/settings', (req,res)=>{
+  let userName = req.session.currentUser.username;
+  User.find({
+    filter: {
+     username: userName
+    }
+   })
+   .then(userData => {
+     res.render('user/settings', { user: userData})
+   })
+   .catch(err => {
+    res.send(`Error: ${err}`);
+    });
+})
+
+// profile
+app.get('/user/profile', (req,res)=>{
+  let userName = req.session.currentUser.username;
+  User.find({
+    filter: {
+     username: userName
+    },
+    project: {
+      firstname: 1,
+      lastname: 1,
+      username:1,
+      password: 1,
+      image: 1
+     }
+   })
+   .then(userData => {
+     res.render('user/feed', { user: userData})
+   })
+   .catch(err => {
+    res.send(`Error: ${err}`);
+    });
+})
+
+
+// Add-friend
+app.get('/add-friend', (req, res) => {
+  let userName = req.session.currentUser.username;
+  User.find({ username: { $ne: userName }})
     .then(usersData => {
       res.render('user/add_friend', {users:usersData});
     })
@@ -158,6 +209,25 @@ app.get('/add_friend', (req, res) => {
     });
 });
 
+app.get('/add-friend/:id', (req,res)=>{
+  debugger
+  const friendId = req.params.id;
+  let userId = req.session.currentUser._id;
+  User.findByIdAndUpdate(friendId,{
+    $push: { friend_request: userId } 
+    })
+    .then(userData => {
+      console.log(`request pushed : ${userData.friend_request}`)
+      res.json({response: "Request sent successfully!"})
+    })
+    .catch(err => {
+      res.send(`Error: ${err}`);
+    });
+
+})
+
+
+// 
 
 
 //Obs, reminder to self >> need to inser app.use(session)... in app.js to get it to work with logout!
