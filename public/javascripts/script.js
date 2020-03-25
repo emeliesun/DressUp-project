7a1e1315eac1be3a19f0fb52bad073083ca81438
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-03-25 02:40:58 +0100

    Added css to nav

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,3 +2,17 @@
 // $loginBtn.addEventListener('click', (event)=>{
 
 // })
+
+
+
+//Navbar links
+// Add active class to the current button (highlight it)
+    var navigation = document.getElementById('navbox');
+    var navlink = navigation.getElementsByClassName('navlink');
+    for (var i = 0; i < navlink.length; i++) {
+        navlink[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
