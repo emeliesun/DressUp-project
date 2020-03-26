8cea169990d68c6b74930233c1f6f98819724a31
Upward-Spiral <azadeh.farshidi@gmail.com>
2020-03-26 01:00:35 +0100

    Added more enums to outfit model

--- models/outfit.js
+++ models/outfit.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const outfitSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String },
-  occasion: { type: String, enum: ['Party', 'Work', 'Dinner'] },
+  occasion: { type: String, enum: ['Party', 'Work', 'Dinner', 'Funeral', 'Date', 'Workout'] },
   description: {type: String},
   items: [{ type: mongoose.Types.ObjectId, ref: "items" }],
   liked_by: [{ type: mongoose.Types.ObjectId, ref: "users" }],
